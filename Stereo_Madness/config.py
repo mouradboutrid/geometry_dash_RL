13603b3dbf2ab212a2c14b80a23060732137f4db
BOUTRID Mourad <m.boutrid@edu.umi.ac.ma>
2025-12-24 17:39:01 +0100

    Update config.py

--- Stereo_Madness/config.py
+++ Stereo_Madness/config.py
@@ -1,11 +1,11 @@
 import os
 import torch
 
-# --- PROJECT SETTINGS ---
+# PROJECT SETTINGS
 PROJECT_NAME = "GD_RL_Agent"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# --- PATHS (Dynamic based on structure) ---
+# PATHS
 CHECKPOINT_DIR = os.path.join(BASE_DIR, "checkpoints")
 LOG_DIR = os.path.join(BASE_DIR, "logs")
 CURRICULUM_FILE = os.path.join(BASE_DIR, "curriculum", "slice_definitions.json")
@@ -15,26 +15,26 @@ TRAIN_LOG = os.path.join(LOG_DIR, "training_log.csv")
 DEATH_LOG = os.path.join(LOG_DIR, "death_log.csv")
 META_FILE = os.path.join(LOG_DIR, "training_meta.json")
 
-# --- SHARED MEMORY ---
+# SHARED MEMORY
 MEM_NAME = "GD_RL_Memory"
-MEM_SIZE_BYTES = 1024  # Ensure this matches your C++ struct size roughly
+MEM_SIZE_BYTES = 1024  # Matches C++ struct size
 
-# --- DEVICE ---
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# DEVICE
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu") # I have only CPU :/
 
-# --- HYPERPARAMETERS ---
+# HYPERPARAMETERS
 GAMMA = 0.99                # Discount Factor (Future reward importance)
 BATCH_SIZE = 64             # Replay Buffer Batch Size
 LR = 0.0003                 # Learning Rate
 MEMORY_SIZE = 50000         # Max Transitions in Buffer
 TARGET_UPDATE = 1000        # Steps between Target Net updates
 
-# --- EXPLORATION (Epsilon Greedy) ---
+# EXPLORATION (Epsilon Greedy)
 EPSILON_START = 1
 EPSILON_END = 0.01
 EPSILON_DECAY = 50000       # Decay over approx 50000 frames
 
-# --- INPUT SHAPE ---
+# INPUT SHAPE
 # 4 Player Vars + (30 Objects * 5 Vars) = 154 inputs
 INPUT_DIM = 154
 OUTPUT_DIM = 2 # (Hold or Release)
